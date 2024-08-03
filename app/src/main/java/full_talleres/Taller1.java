b6665fb0511f9a059adb6028d56ca8a86c1ab4eb
Juan Jose Alvarez Restrepo <150312930+restrepo0510@users.noreply.github.com>
2024-08-02 21:02:40 -0500

    Update Taller1.java

--- app/src/main/java/full_talleres/Taller1.java
+++ app/src/main/java/full_talleres/Taller1.java
@@ -27,6 +27,21 @@ public class Taller1 {
      y la convierta a metros/seg y a metros/hora. Retorne un string del tipo: ### m/s - ### m/h. Si hay error, 
      devuelva el string "Error en la conversion"
     */
+    public static String Convertir_km_seg(double velocidad) {
+
+        try {
+            double m_s = velocidad * 1000;
+
+            double m_h = velocidad * 1000 * 3600;
+
+            String resultado = m_s + " m/s - " + m_h + " m/h";
+
+            return resultado;
+        } catch (Exception e) {
+
+            return "error en la conversión";
+        }
+    }
     
     
 
@@ -34,13 +49,34 @@ public class Taller1 {
     cc (centímetros cúbicos) y devuelva un float con su equivalente en litros. Si hay algún error, retorne 0.
     */
   
-    
+    public static float Convertir_cm_lt(double cc) {
+
+        try {
+            float lt = (float) cc * 1 / 1000;
+            return lt;
+
+        } catch (Exception e) {
+            return 0;
+
+        }
+    }
 
   
    /*4.	Diseñe un algoritmo e implemente la función Convertir_us_cops que reciba una cantidad entera de dólares 
    y devuelva su equivalente en pesos usando una TRM de $4170 pesos por cada dólar. La función recibe enteros
    y devuelve enteros, pueden ser grandes. Si hay algún error, retorne -1.
    */
+
+    public static long Convertir_us_cops(int usd) {
+
+        try {
+            long cop = usd * 4170;
+            return cop;
+        } catch (Exception e) {
+            return -1;
+        }
+
+    }
    
     
   
@@ -49,6 +85,16 @@ public class Taller1 {
    centígrados y la devuelve en grados Fahrenheit (averiguar la fórmula) F = 32 + ( 9 * C / 5). Si hay algún error, 
    retorne 0.
    */
+    public static float Convertir_cent_far(float g_centigrados) {
+
+        try {
+            float fahrenheit = 32 + (9 * g_centigrados / 5);
+            return fahrenheit;
+        } catch (Exception e) {
+            return 0;
+        }
+
+    }
 
           
 
@@ -58,6 +104,21 @@ public class Taller1 {
    Si hay algún error, devuelva -1.
    */
 
+    public static int Calcular_segs(short dia, short hora, short min, short seg) {
+
+        try {
+
+            int seg_s= 0;
+            seg_s = (dia * 60 * 60 * 24) + (hora * 60 * 60) + (min * 60) + seg;
+            return seg_s;
+
+        } catch (Exception e) {
+            return -1;
+        }
+
+    }
+
+
    
 
    /*7.	Un usuario tiene un sistema de báscula para pesar camiones. Diseñe un algoritmo e implemente la función 
@@ -66,6 +127,20 @@ public class Taller1 {
    en un string del tipo: "### kilos - ### toneladas". 
    Si hay algún error, devuelva en un string "Error en la función Calcular_peso_carga"*/
 
+    public static String Calcular_peso_carga(float camion_cargado, float camion_vacio) {
+
+        try {
+            float t_ton = camion_cargado - camion_vacio;
+            float t_kg = t_ton * 1000;
+            String resultado = t_kg + " kilos - " + t_ton + " toneladas";
+
+            return resultado;
+        } catch (Exception e) {
+            return ("Error en la función Calcular_peso_carga");
+        }
+
+    }
+
    
 
    /*8.	Diseñe un algoritmo e implemente la función Calcular_horasxviaje que calcule y devuelva un float con las horas  
@@ -73,6 +148,19 @@ public class Taller1 {
    otro dato short que es la velocidad  promedio que alcanzará también el vehículo en kilómetros/hora.  
    Si hay algún error, devuelva -1.
    */
+    public static float Calcular_horasxviaje(String destino, short d_kms, short vel_prom) {
+
+        try {
+
+            float tiempo = (short) d_kms / vel_prom;
+            return tiempo;
+
+        } catch (Exception e) {
+            return -1;
+        }
+
+    }
+
 
   
    
@@ -86,6 +174,26 @@ public class Taller1 {
    
    Si hay algún error, devuelva -1.*/
 
+    public static float Calcular_combustible(int[] rutas) {
+
+        try {
+
+            double consumo_despegue = 1.2f;
+            double consumo_aterrizaje = 0.4f;
+            double consumo_km = 0.2f / 60.8f;
+            double total_combustible = 0;
+
+            for (int ruta : rutas) {
+                total_combustible += consumo_despegue + consumo_aterrizaje + (ruta * consumo_km);
+            }
+
+            return total_combustible;
+        } catch (Exception e) {
+            return -1;
+        }
+        
+    }
+
    
    
    
@@ -96,6 +204,18 @@ public class Taller1 {
    
    Si hay algún error, devuelva 0.
    */
+    public static double Calcular_peso_luna(byte peso_kilos) {
+
+        try {
+            double peso_tierra_new = peso_kilos * 9.81;
+            double peso_luna_new = peso_tierra_new * 0.165;
+            return peso_luna_new;
+
+        } catch (Exception e) {
+            return 0;
+        }
+
+    }
    
    
    
